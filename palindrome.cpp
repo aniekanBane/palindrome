7952cfd42df3508830fde633fac639672e853bff
Aniekan Umanah <77311836+aniekanBane@users.noreply.github.com>
2021-09-24 05:38:40 +0100

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+pair<string, int> isPalindrome(string s){
+
+  int len = s.length();
+  string proc;
+  for (char c : s){
+    if (!isspace(c) && !ispunct(c))
+       proc +=  tolower(c);
+  }
+  //cout << proc;
+  string rev;
+  for (int n = proc.length()-1; n >=0; n--){
+     rev.push_back(proc[n]);
+  }
+  if (proc == rev) return pair("True", len);
+  else return pair("False", 0);
+}
+
+int main() {
+  string str;
+  cout << "Type your sentence or word" << endl;
+  do{
+    cin >> str;
+    pair<string, int> pal = isPalindrome(str);
+    cout << "Palindrome : " << pal.first <<", Lenght : " << pal.second;
+  } while (str != "exit");
+}
